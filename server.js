f10c89ed8ec54c859eb38a3afad809b3a4722f29
dwarfsuomalainen <r.bogoudinov@mac.com>
2022-06-16 17:42:33 +0300

    task2

--- server.js
+++ server.js
@@ -7,6 +7,8 @@
 const express = require("express");
 const path = require('path');
 const recipes = require('./Data_recipes');
+const uuid = require('uuid');
+
 
 
 const app = express();
@@ -14,6 +16,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
+
 
 app.get('/recipe/:food', (req, res)=> {
     res.json({name: req.params.food});
@@ -24,8 +27,14 @@ app.get('/recipe/:food', (req, res)=> {
 // create arecipe
 app.post('/recipe', (req, res)=> {
    const newRecipe = {
-    
+    id: uuid.v4(),
+    name: req.body.name,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions
+
    } 
+   recipes.push(newRecipe);
+   res.json(recipes);
 })
 
 
